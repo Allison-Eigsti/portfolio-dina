e6ab7e9625b0679dc1778a262ef6b6a603a99f58
AllisonE-code <allisonmeigsti@gmail.com>
2026-07-19 18:37:39 +0200

    Added logger, notFound and serverError middleware

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 require('dotenv').config()
+
 const express = require('express')
-
-//Import routers here
-const projectRouter = require('./routes/projectRouter.js')
-
-const connectDB = require('./config/db.js')
-connectDB()
-
 const app = express()
 const PORT = process.env.PORT || 3000
+const connectDB = require('./config/db.js')
+const projectRouter = require('./routes/projectRouter.js')
+const logger = require('./middleware/logger.js')
+const serverError = require('./middleware/server-error')
+const notFound = require('./middleware/not-found')
+
+
+connectDB()
 
 app.use(express.json())
+app.use(logger)
 
 app.get('/', (req, res) => {
     console.log('Hello World')
 })
 
 app.use('/projects', projectRouter)
+
+app.use(serverError)
+app.use(notFound)
 
 //connect routers here
 
