bd622f9b80c44d5437f74873f5b5cffe70dc6948
AllisonE-code <allisonmeigsti@gmail.com>
2026-09-13 15:36:04 +0200

    changed order of middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,16 +30,16 @@ connectDB()
 
 
 // Middleware
-app.use(express.json())
-app.use(logger)
-app.use(helmet())
-
 app.use(
   cors({
     origin: 'http://localhost:5173',
     credentials: true
   })
 )
+app.use(express.json())
+app.use(logger)
+app.use(helmet())
+
 
 // Unprotected Routes
 app.use('/auth', authRouter)
